4c29f705c80bf14f47c6b2e512cd743ece1604ab
Elteclaa <147396191+Elteclaa@users.noreply.github.com>
2023-10-25 01:52:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-require('dotenv').config();
-require('better-logging')(console);
 
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const server = require('http').Server(app);
+const fs = require('fs');
+const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const betterLogging = require('better-logging');
+
+betterLogging(console);
 
 const { renderView } = require('./src/helpers/renderView');
 
-app.set('view engine', 'ejs');
+const app = express();
+const server = http.Server(app);
 
-// Agregamos rutas al servidor
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     renderView(res, 'calculator');
 });
 
-// Encendemos el servidor
+app.get('/mostrarHistorial', (req, res) => {
+    try {
+        const historialData = fs.readFileSync('historial.json', 'utf-8');
+        const historial = JSON.parse(historialData);
+        res.json({ historial });
+    } catch (error) {
+        console.error('Error al cargar el historial:', error);
+        res.status(500).json({ error: 'Error al cargar el historial' });
+    }
+});
+
+app.post('/HistorialAdd', (req, res) => {
+    console.log(req.body);
+    let registro = req.body;
+
+    let historial = [];
+    try {
+        const historialData = fs.readFileSync('historial.json', 'utf-8');
+        historial = JSON.parse(historialData);
+    } catch (err) {
+        console.log('Error al leer el historial');
+    }
+
+    historial.push(registro);
+    fs.writeFileSync('historial.json', JSON.stringify(historial, null, 2));
+});
+
 server.listen(process.env.PORT, () => {
-    console.info(`Servidor corriendo en el puerto ${process.env.PORT}`);
+    console.info(`Servidor corriendo en el puerto: http://localhost:${process.env.PORT}`);
 });
