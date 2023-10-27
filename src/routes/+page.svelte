ab3851652081d7e1f8f152b75012febc28d4cc9b
Dust <dustparticle@paranoici.org>
2023-10-27 18:04:55 -0300

    Terminado

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,96 +1,96 @@
-<div class="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-purple-900 shadow-lg border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
+<div class="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-gray-900 shadow-lg border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
     <div class="">
-      <div class="p-5 text-white text-center text-3xl bg-purple-900"><span class="text-orange-500">Calcu</span>lator</div>
-      <div class="p-5 pb-5 text-white text-right text-3xl bg-purple-800">
-        <input class="input input-ghost bg-purple-800" bind:value={pantalla}>
+      <div class="p-5 text-white text-center text-3xl bg-gray-900"><span class="text-orange-500">Calcu</span>Torres</div>
+      <div class="p-5 pb-5 text-white text-right text-3xl bg-gray-800">
+        <input class="input input-ghost bg-gray-800 text-end" bind:value={pantalla}>
       </div>
       
       
- <div class="flex items-stretch bg-purple-900 h-24">
+ <div class="flex items-stretch bg-gray-900 h-24">
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">%</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => procesar("C")}>C</button>
     </div>
   
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">(</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("(")}>(</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">)</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo(")")}>)</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">÷</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("÷")}>÷</button>
     </div>
 </div>
       
-<div class="flex items-stretch bg-purple-900 h-24">
+<div class="flex items-stretch bg-gray-900 h-24">
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">7</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("7")}>7</button>
     </div>
   
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">8</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("8")}>8</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">9</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("9")}>9</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">×</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("*")}>×</button>
     </div>
 </div>
       
-<div class="flex items-stretch bg-purple-900 h-24">
+<div class="flex items-stretch bg-gray-900 h-24">
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">4</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("4")}>4</button>
     </div>
   
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">5</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("5")}>5</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">6</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("6")}>6</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">-</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("-")}>-</button>
     </div>
 </div>
       
-<div class="flex items-stretch bg-purple-900 h-24">
+<div class="flex items-stretch bg-gray-900 h-24">
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">1</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("1")}>1</button>
     </div>
   
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">2</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("2")}>2</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">3</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("3")}>3</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">+</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("+")}>+</button>
     </div>
 </div>
       
 
-<div class="flex items-stretch bg-purple-900 h-24 mb-4">
+<div class="flex items-stretch bg-gray-900 h-24 mb-4">
     
     <div class="flex-1 pl-7 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-52 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">0</div>
+      <button class="rounded-full h-20 w-52 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo("0")}>0</button>
     </div>
    
     <div class="flex-1 pl-7 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-purple-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">.</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => agregar_simbolo(".")}>.</button>
     </div>
    
     <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-      <div class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none">=</div>
+      <button class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg border-2 border-purple-700 hover:border-2 hover:border-gray-500 focus:outline-none" on:click={() => procesar("=")}>=</button>
     </div>
 </div>
       
@@ -98,8 +98,22 @@
   </div>
 </div>
 <script>
-    let pantalla = ""
-    function a(){
+  let pantalla = '';
 
+  function agregar_simbolo(simbolo){
+    pantalla += simbolo
+  }
+  function procesar(value) {
+    if (value === '=') {
+      try {
+        pantalla = eval(pantalla);
+      } catch (error) {
+        pantalla = 'Error';
+      }
+    } else if (value === 'C') {
+      pantalla = '';
+    } else {
+      pantalla += value;
     }
+  }
 </script>
